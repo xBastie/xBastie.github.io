cb07e1c14a18517c4f2e7c79af6c9100fb60fedc
xBastie <154934468+xBastie@users.noreply.github.com>
2024-01-05 05:07:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
       var userInput = document.getElementById('userInput').value.toLowerCase();
 
       if (userInput === 'wilhelminenstraße') {
-        document.getElementById('output').innerHTML = '51.076303 13.750260';
+        document.getElementById('output').innerHTML = '51.07630 13.75026';
         document.getElementById('output').classList.add('correctOutput'); // Hinzufügen der Klasse für richtige Ausgabe
       } else {
         document.getElementById('output').innerHTML = 'Falsche Eingabe. Versuche es erneut.';
